4fad9d640cfc238f49536adcf84ddfa0ec3f5b27
Ana Nikolova <ani.nik19@gmail.com>
2020-01-22 07:56:43 +0100

    generating rando shapes

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const {
   Mouse
 } = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   optionns: {
-    width: 800,
-    height: 600
+    wireframes: false,
+    width,
+    height
   }
 });
 Render.run(render);
@@ -46,4 +50,21 @@ const walls = [
 
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random Shapes
+for (let i = 0; i < 40; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+        render: {
+          fillStyle: "red"
+        }
+      })
+    );
+  }
+}
