12c61d430589b87a68d52d82ad2207471773997a
Ana Nikolova <ani.nik19@gmail.com>
2020-01-27 06:01:27 +0100

    display a success message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 14;
-const cellsVertical = 10;
+const cellsHorizontal = 5;
+const cellsVertical = 5;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
@@ -15,7 +15,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: true,
+    wireframes: false,
     width,
     height
   }
@@ -123,7 +123,13 @@ horizontals.forEach((row, rowIndex) => {
       rowIndex * unitLengthY + unitLengthY,
       unitLengthX,
       10,
-      { label: "wall", isStatic: true }
+      {
+        label: "wall",
+        isStatic: true,
+        render: {
+          fillStyle: "red"
+        }
+      }
     );
     World.add(world, wall);
   });
@@ -139,7 +145,13 @@ verticals.forEach((row, rowIndex) => {
       rowIndex * unitLengthY + unitLengthY / 2,
       10,
       unitLengthY,
-      { label: "wall", isStatic: true }
+      {
+        label: "wall",
+        isStatic: true,
+        render: {
+          fillStyle: "red"
+        }
+      }
     );
     World.add(world, wall);
   });
@@ -154,7 +166,10 @@ const goal = Bodies.rectangle(
   unitLengthY * 0.7,
   {
     label: "goal",
-    isStatic: true
+    isStatic: true,
+    render: {
+      fillStyle: "green"
+    }
   }
 );
 World.add(world, goal);
@@ -162,7 +177,10 @@ World.add(world, goal);
 // Ball
 const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
-  label: "ball"
+  label: "ball",
+  render: {
+    fillStyle: "blue"
+  }
 });
 World.add(world, ball);
 
@@ -191,6 +209,7 @@ Events.on(engine, "collisionStart", event => {
       labels.includes(collision.bodyA.label) &&
       labels.includes(collision.bodyB.label)
     ) {
+      document.querySelector(".winner").classList.remove("hidden");
       world.gravity.y = 1;
       world.bodies.forEach(body => {
         if (body.label === "wall") {
